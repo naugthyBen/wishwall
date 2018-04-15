161808f123b42ca8aa285482c5cebe656fe87932
Ben <2446179295@qq.com>
2018-04-15 21:10:51 +0800

    许愿界面添加了蒙版和提示，选择匿名发布消息提示，字数限制提示已完成。

--- wishwall/index.js
+++ wishwall/index.js
@@ -1,83 +1,44 @@
 $(document).ready(function() {
-    idContentchange();
-    indexContentchange();
-    clickHeart();
-    clickChangeImg();
+    idContentchange();       // 用户界面心愿、评论切换
+    indexContentchange();    // 许愿主页热门许愿、本周最新、热门话题
+    clickHeart();          //点赞
+    clickChangeImg();    //点击更换头部背景图
     // clickdel();
     // nodel();
     // yesdel();
-    hideuser();
-    // hidewishuser();
+    hideuser();   //匿名评论
+    hidewishuser();//匿名发布
+    getit();  //我知道了
+    limit();
 });
 // 用户界面心愿、评论切换
 function idContentchange(){
-    var $idContent_li =$(".id_tab_menu ul li");
+    let $idContent_li =$(".id_tab_menu ul li");
     $idContent_li.click(function(){
         $(this).addClass("id_selected")
             .siblings().removeClass("id_selected");
-        var index = $idContent_li.index(this);
+        let index = $idContent_li.index(this);
         $(".id_tab_box>div")
             .eq(index).show()
             .siblings().hide();
     })
 }
 
-
 // 许愿主页热门许愿、本周最新、热门话题
 function indexContentchange(){
-    var $indexContent_li =$(".index_tab_menu ul li");
+    let $indexContent_li =$(".index_tab_menu ul li");
     $indexContent_li.click(function(){
         $(this).addClass("index_selected")
             .siblings().removeClass("index_selected");
-        var index = $indexContent_li.index(this);
+        let index = $indexContent_li.index(this);
         $(".index_tab_box>div")
             .eq(index).show()
             .siblings().hide();
     })
 }
 
-var clickdel=(function clickdel(){
-    var fn;   
-    $(".del").bind("click",function abc(){
-        $(".delif").show();
-        $(".delBox").show();
-        console.log($(this).parent().parent(".bgc"));
-        function del(){
-            $(this).parent().parent(".bgc").hide();
-        }
-        fn=del;
-        console.log(fn);
-        // return {
-        //     del:del
-        // }
-    });
-   function nodel(){
-        $(".nodel").bind('click', function() {
-            $(".delif").hide();
-            $(".delBox").hide();
-        });
-    }
-    function yesdel(){
-        $(".yesdel").bind('click',function (){
-            fn();
-           $(".delif").hide();
-           $(".delBox").hide();
-           
-           // console.log(fn());
-           console.log("3");
-        });
-    }
-    return{
-        nodel:nodel,
-        yesdel:yesdel
-    }
-})();
-clickdel.nodel();
-clickdel.yesdel();
-// 
-// 
-// 
-// function clickdel(){
+// var clickdel=(function clickdel(){
+//     var fn;   
 //     $(".del").bind("click",function abc(){
 //         $(".delif").show();
 //         $(".delBox").show();
@@ -85,70 +46,63 @@ clickdel.yesdel();
 //         function del(){
 //             $(this).parent().parent(".bgc").hide();
 //         }
-//         // fn=del;
-//         // console.log(fn);
-//         return {
-//             del:del
-//         }
+//       return del;
 //     });
+//     fn = del;
+//    function nodel(){
+//         $(".nodel").bind('click', function() {
+//             $(".delif").hide();
+//             $(".delBox").hide();
+//         });
+//     }
+//     function yesdel(){
+//         $(".yesdel").bind('click',function (){
+//             fn();
+//            $(".delif").hide();
+//            $(".delBox").hide();
+           
+//            // console.log(fn());
+//            console.log("3");
+//         });
+//     }
+//     return{
+//         nodel:nodel,
+//         yesdel:yesdel
+//     }
+// })();
+// clickdel.nodel();
+// clickdel.yesdel();
+
+
+
+// function clickdel(){
+//     var delt=( $(".del").bind("click",function (){
+//         $(".delif").show();
+//         $(".delBox").show();
+//         // console.log($(this).parent().parent(".bgc"));
+//         return function del(){
+//             $(this).parent().parent(".bgc").hide();
+//         }
+//     })();
+//     fn=del;
 // }
+
+// // var clickd=clickdel();
 // function nodel(){
 //     $(".nodel").bind('click', function() {
 //         $(".delif").hide();
 //         $(".delBox").hide();
 //     });
 // }
+
 // function yesdel(){
 //     $(".yesdel").bind('click',function (){
 //        $(".delif").hide();
 //        $(".delBox").hide();
-//        // console.log(fn());
-//        // console.log("3");
-//        // $(".del").parent().parent(".bgc").hide();
+//        fn();
 //     });
 // }
 
-// $(function (){
-//     $(".hideuser").toggle( function() {
-//         $(this).toggle();         
-//     },function(){
-//         $(this).toggle();
-//     })
-// });
-
-
-
-//点击更换图标事件
-// function changeTypeIcon(){
-//     $(".wishtype_message").click(
-//         function (){
-//             var tnext=$(this).next();
-//             var tyepLabel=tnext.attr('id');
-//             if(tyepLabel=='sayhi_label'){
-//                 tnext.css('background-image','url(./img/sayhi_type_yes.png)');
-//                 $("#pray_label").css('background-image','url(./img/pray_type_no.png)');
-//                 $("#complain_label").css('background-image','url(./img/complain_type_no.png)');
-//                 $("#default_label").css('background-image','url(./img/default_type_no.png)');
-//             }else if(tyepLabel=='pray_label'){
-//                 tnext.css('background-image','url(./img/pray_type_yes.png)');
-//                 $("#sayhi_label").css('background-image','url(./img/sayhi_type_no.png)');
-//                 $("#complain_label").css('background-image','url(./img/complain_type_no.png)');
-//                 $("#default_label").css('background-image','url(./img/default_type_no.png)');
-//             }else if(tyepLabel=='complain_label'){
-//                 tnext.css('background-image','url(./img/complain_type_yes.png)');
-//                 $("#sayhi_label").css('background-image','url(./img/sayhi_type_no.png)');
-//                 $("#pray_label").css('background-image','url(./img/pray_type_no.png)');
-//                 $("#default_label").css('background-image','url(./img/default_type_no.png)');
-//             }else if(tyepLabel=='default_label'){
-//                 tnext.css('background-image','url(./img/default_type_yes.png)');
-//                 $("#sayhi_label").css('background-image','url(./img/sayhi_type_no.png');
-//                 $("#pray_label").css('background-image','url(./img/pray_type_no.png)');
-//                 $("#complain_label").css('background-image','url(./img/complain_type_no.png)');
-//             }
-            
-//         }
-//     );
-// }
 
 //匿名评论
 function hideuser(){
@@ -163,27 +117,47 @@ function hideuser(){
     });
 }
 //匿名发布
-// function hidewishuser(){
-//     $(".hidewishuser").bind('click', function() {
-//         let a=1;
-//         console.log("1");
-//         if($(".cancelhide").is(":visible")){
-//             // $(".choosedhide").hide();
-//             $(".canceledhide").show(600);
-//             $(".hidewishuser").addClass("hidename");         
-//         }else{
-//             $(".canceledhide").show(600);
-//             // $(".choosedhide").hide();
-//             $(".hidewishuser").removeClass("hidename");  
-//         }
-//         a=2;
-//     });
-// }
-$(".hidewishuser").toggle(function(){
-    $("choosedhide").show();
-},function(){
-    $("canceledhide").show();
-});
+function hidewishuser(){
+    $(".hidewishuser").toggle( function() {
+            $(".choosedhide").show();
+            $(".canceledhide").hide();
+            $(".hidewishuser").addClass("hidename"); 
+            setTimeout(function(){
+                $(".choosedhide").hide();
+                $(".canceledhide").hide();
+            }, 1000);        
+        },function(){
+            $(".canceledhide").show();
+            $(".choosedhide").hide();
+            $(".hidewishuser").removeClass("hidename");  
+             setTimeout(function(){
+                $(".choosedhide").hide();
+                $(".canceledhide").hide();
+            }, 1000);
+        
+    });
+}
+//我知道了
+function getit(){
+    $(".getit").toggle(function(){
+        $(".niming").hide();
+        $(".remen").show();
+    },function(){
+        $(".remen").hide();
+        $(".uncom").show();
+    },function(){
+        $(".uncom").hide();
+        $(".mengban").hide();
+    })
+}
+//字数限制
+function limit(){
+    $(".targetip>input").keyup(function() {
+        if ($.trim(this.value).length > 11) { alert("输入字符大于11个,请重新输入!"); }
+    });
+}
+
+// 详情页不许评论
 //点赞
 function clickHeart(){
     $(".love>img").live("click",function (){
@@ -199,15 +173,6 @@ function clickHeart(){
             $(this).attr('src','img/loved.png');
             $(this).siblings().eq(0).text((loveNum+1));                             
         }
-        
-        //  $.ajax({
-        //      url: 'enter/homePage_doZan.action?targetMid='+mid,     //要访问的service路径
-        //      type: "get",        //访问实现的方式:get 或 post
-        //      async:true,      //是否异步
-        //      cache:false,      //是否缓存
-        //      success:function(result){    //请求成功后执行
-        //      }
-        // });
     });
 }
 
@@ -264,31 +229,10 @@ function changeTypeIcon(){
         }
     );
 }
-// $(".message").each(function(){
-//   var maxwidth=61;
-//   if($(this).text().length > maxwidth){
-//     $(this).text($(this).text().substring(0,maxwidth));
-//     $(this).html($(this).html()+'...');
-//   }
-// });
-
-// var wordLimit=function(){
-//     $(".message").each(function(){
-//         var copyThis = $(this.cloneNode(true)).hide().css({
-//             'position': 'absolute',
-//             'width': 'auto',
-//             'overflow': 'visible'
-//         });	
-//         $(this).after(copyThis);
-//         if(copyThis.width()>$(this).width()){
-//             $(this).text($(this).text().substring(0,$(this).html().length-4));
-//             $(this).html($(this).html()+'...');
-//             copyThis.remove();
-//             wordLimit();
-//         }else{
-//             copyThis.remove(); //清除复制
-//             return;
-//         }
-//     });
-// }
-// wordLimit();
+$(".message").each(function(){
+  var maxwidth=61;
+  if($(this).text().length > maxwidth){
+    $(this).text($(this).text().substring(0,maxwidth));
+    $(this).html($(this).html()+'...');
+  }
+});
